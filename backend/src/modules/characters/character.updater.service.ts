e438a4b2aef55c5994ee957f642b3a4b1ef66482
Renab <renab@faucher.co>
2026-02-11 22:31:13 -0500

    Fix the Private Keys

--- backend/src/modules/characters/character.updater.service.ts
+++ backend/src/modules/characters/character.updater.service.ts
@@ -39,7 +39,7 @@ export class CharacterUpdaterService {
 
       const serviceAccountAuth = new JWT({
         email:  CharacterUpdaterService.config.get('GOOGLE_API_EMAIL'),
-        key:    CharacterUpdaterService.config.get('GOOGLE_API_KEY'),
+        key:    CharacterUpdaterService.config.get('GOOGLE_API_KEY').replace(/\\n/g, '\n'),
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
       });
       this.logger.log("Connecting to Google Sheets")
